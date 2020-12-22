21c2c68d487132486111849ebf116f938fbc3d30
Yvonne Chen <yvonnechen1991@gmail.com>
2020-12-22 04:39:29 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,12 @@
 def new_hash
   # return an empty hash
+  new_hash = Hash.new
+  new_hash
 end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+  
 end
 
 def pioneer
