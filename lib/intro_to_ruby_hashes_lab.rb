11ee0b06295a6944fe6196c8e2db6f5da76cfcb7
Yvonne Chen <yvonnechen1991@gmail.com>
2020-12-22 04:47:44 +0000

    Done.

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,28 @@
 def new_hash
   # return an empty hash
+  Hash.new
+  # {}
 end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+  {
+    :first_name => "Yvonne",
+    :last_name => "Chen"
+  }
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  {
+    :name => "Grace Hopper"
+  }
 end
 
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
+  {
+    :id => number
+  }
 end
